300c23c82b3ac5e89e3ec7478f19204664bf03d9
Julio Campagnolo <juliocampagnolo@gmail.com>
2022-03-26 13:00:41 -0300

    remove old catalog api

--- astropop/catalogs/__init__.py
+++ astropop/catalogs/__init__.py
@@ -1,28 +1,13 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 """Catalog managing and query."""
 
-from .vizier import VizierCatalogClass, \
-                    UCAC4Catalog, UCAC5Catalog, GSC23Catalog, \
-                    APASSCalatolg, DENISCatalog, TWOMASSCatalog
-from .simbad import SimbadCatalog, SimbadCatalogClass
-from .local import TableCatalog, ASCIICatalog
 from .utils import identify_stars
 
 
-__all__ = ['identify_stars', 'catalogs_available',
-           'VizierCatalogClass', 'SimbadCatalogClass', 'UCAC5Catalog',
-           'SimbadCatalog', 'UCAC4Catalog', 'GSC23Catalog', 'APASSCalatolg',
-           'DENISCatalog', 'TWOMASSCatalog', 'default_catalogs',
-           'TableCatalog', 'ASCIICatalog', 'FITSCatalog']
+__all__ = ['identify_stars', 'catalogs_available']
 
 
-default_catalogs = {'Simbad': SimbadCatalog,
-                    'UCAC4': UCAC4Catalog,
-                    'UCAC5': UCAC5Catalog,
-                    'APASS': APASSCalatolg,
-                    'DENIS': DENISCatalog,
-                    '2MASS': TWOMASSCatalog,
-                    'GSC2.3': GSC23Catalog}
+default_catalogs = {}
 
 
 catalogs_available = default_catalogs.keys()
